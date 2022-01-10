Fixed header logic in average_xi.py

gaussian_s4PCF/python/average_xi.py
@@ -22,10 +22,10 @@ for i in range(1, ndata+1):
     arrays.append(np.loadtxt(filename, skiprows=2)) # read data
     with open(filename) as f:
         new_header = ''.join(f.readlines()[:2]).strip() # read first two lines (have different format)
-        if new_header != header: # check that header does not change suddenly
-            print(f"Warning: header (first two lines) of {filename} differs from the first header")
-        elif header is None:
+        if header is None:
             header = new_header # save first header
+        elif new_header != header: # check that header does not change suddenly
+            print(f"Warning: header (first two lines) of {filename} differs from the first header")
 arrays = np.array(arrays)
 out = np.mean(arrays, axis=0) # output simple average of corrfuncion, assumes about equal size of data/mock files
 dirname = os.path.join(workdir, f"avg_{ndata}")