ed49165bd8ed58d4a34b5a488cf75d4ed75df1ec
Michael Rashkovetskyi <misharash@gmail.com>
2022-01-10 17:53:41 +0200

    Change xi & output files

--- gaussian_s4PCF/modules/parameters.h
+++ gaussian_s4PCF/modules/parameters.h
@@ -21,7 +21,7 @@ public:
 
     // The name of the correlation function file for the first set of particles
 	char *corname = NULL;
-	const char default_corname[500] = "xi_x50/0001/xi_n900_m10_11.dat";
+	const char default_corname[500] = "xi_x50/avg_50/xi_n900_m10_11.dat";
 
     // Name of the correlation function radial binning .csv file
     char *radial_bin_file_long = NULL;
@@ -36,7 +36,7 @@ public:
 
     // Output directory
     char *out_file = NULL;
-    const char default_out_file[500] = "./out_xi_n900_m10";
+    const char default_out_file[500] = "./out_xi_x50_n900_m10";
 
     // The number of mu bins in the correlation function
     int mbin_cf = 10;
