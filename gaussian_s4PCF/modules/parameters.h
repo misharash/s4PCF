41a55ed16e42b4c32e9303a9774803966545aebc
Michael Rashkovetskyi <misharash@gmail.com>
2022-01-22 15:58:35 -0500

    More threads & loops for disconnected calculation

--- gaussian_s4PCF/modules/parameters.h
+++ gaussian_s4PCF/modules/parameters.h
@@ -42,7 +42,7 @@ public:
     int mbin_cf = 10;
 
     // The number of threads to run on
-	int nthread = 10;
+	int nthread = 30;
 
     // The grid size, which should be tuned to match boxsize and rmax.
 	// This uses the maximum width of the cuboidal box.
@@ -63,7 +63,7 @@ public:
     //---------- PRECISION PARAMETERS ---------------------------------------
 
     // Maximum number of iterations to compute the C_ab integrals over
-    int max_loops = 10;
+    int max_loops = 90;
 
     // Number of random cells to draw at each stage
     int N2 = 20; // number of j cells per i cell
