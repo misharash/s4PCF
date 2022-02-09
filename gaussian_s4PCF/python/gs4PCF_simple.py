afaf6d2e5d87de915e609b7872d37e9a55e53c49
Michael Rashkovetskyi <misharash@gmail.com>
2022-02-09 00:59:43 -0500

    Pass precision to outer integral

--- gaussian_s4PCF/python/gs4PCF_simple.py
+++ gaussian_s4PCF/python/gs4PCF_simple.py
@@ -101,7 +101,7 @@ def inner_integrand(rij, rb_min, rb_max, rc_min, rc_max):
     return value
 
 def integrate_gs4PCF(ra_min, ra_max, rb_min, rb_max, rc_min, rc_max):
-    return romberg(inner_integrand, ra_min, ra_max, args=(rb_min, rb_max, rc_min, rc_max))
+    return romberg(inner_integrand, ra_min, ra_max, args=(rb_min, rb_max, rc_min, rc_max), rtol=precision)
 
 for i, (ra_min, ra_max) in enumerate(long_bins):
     print(f"Started {i+1} of {len(long_bins)} ({datetime.now()})")
