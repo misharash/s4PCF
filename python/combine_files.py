79bd87abd8d8b0cd87b4710ebcab586519ef7626
Michael Rashkovetskyi <misharash@gmail.com>
2022-02-19 13:22:26 -0500

    Fixed mu bin counting when there is only one

--- python/combine_files.py
+++ python/combine_files.py
@@ -56,7 +56,7 @@ for N in Ns:
         bins = np.loadtxt(DmR_file, max_rows=n-1)
         if N == "fine2":
             bins = np.arange(len(bins))[:, None] # in this case "bins" is midpoints, need to override with bin numbers and add second dim
-            n_mu = len(np.loadtxt(DmR_file, skiprows=1, max_rows=1)) # count mu bins
+            n_mu = len(np.loadtxt(DmR_file, skiprows=1, max_rows=1, ndmin=1)) # count mu bins: read 2nd line of file to an 1D array and take its length
             countsR = 0.5 * bin_volume_cf(bins, np.max(bins)) / n_mu # factor of 1/2 because we count only i > j pairs for fine2pcf
         elif N == "2":
             countsR = bin_volume_short(bins, np.max(bins))
