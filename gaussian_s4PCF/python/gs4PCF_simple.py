Fixed normalization in xi_kl averaging

gaussian_s4PCF/python/gs4PCF_simple.py
@@ -114,7 +114,7 @@ def inner_integrand(rij, rb_min, rb_max, rc_min, rc_max):
     xi_kl_bcavg = 0
     for (a, b) in zip(points_of_interest[:-1], points_of_interest[1:]):
         xi_kl_bcavg += integration_wrapper(lambda R: R*xi_fun(R)*G(rij, R, rb_min, rb_max, rc_min, rc_max), a, b)
-    xi_kl_bcavg *= 3 / (16 * rij * (pow(rb_max, 3) - pow(rb_min, 3)) * (pow(rc_max, 3) - pow(rc_min, 3))) # common factor
+    xi_kl_bcavg *= 9 / (16 * rij * (pow(rb_max, 3) - pow(rb_min, 3)) * (pow(rc_max, 3) - pow(rc_min, 3))) # common factor
     value += xi_ik * xi_kl_bcavg
     return value * np.square(rij) # r_ij^2 weighting for bin average
 