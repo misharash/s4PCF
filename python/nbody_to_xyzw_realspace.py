ea61fcfaa9d257b4e5b5129dd1f14db63a123959
Michael Rashkovetskyi <misharash@gmail.com>
2022-03-25 15:29:20 -0400

    Added fits support to input converter

--- python/nbody_to_xyzw_realspace.py
+++ python/nbody_to_xyzw_realspace.py
@@ -1,10 +1,10 @@
 ### nbody_to_xyzw_realspace.py (Michael Rashkovetskyi, with the help of Boryana Hadzhiyska, 2022)
-# This reads an ECSV file (as at least in AbacusSummit mocks) and converts it to xyzw plain text format assumed by s4PCF code & running script
+# This reads an ECSV or FITS file (as at least in some AbacusSummit mocks) and converts it to xyzw plain text format assumed by s4PCF code & running script
 # Output contains real-space (not redshift-space) coordinates
 
 import sys
 import numpy as np
-from astropy.io import ascii
+from astropy.io import ascii, fits
 
 if len(sys.argv) != 4:
     raise Exception("Need to specify input file, box size and output file!")
@@ -13,7 +13,11 @@ else:
     boxsize = float(sys.argv[2])
     outfile = sys.argv[3]
 
-gals_arr = ascii.read(infile) # contains x, y, z and so on, column titles specified in first non-comment string
+if infile.endswith(".fits"): # FITS file
+    f = fits.open(infile)
+    gals_arr = f[1].data # contains x, y, z and so on
+else: # ECSV
+    gals_arr = ascii.read(infile) # contains x, y, z and so on, column titles specified in first non-comment string
 # stack 3 coordinate columns and weights of 1 for each galaxy, transpose
 gals_pos_weights = np.vstack((gals_arr['x'], gals_arr['y'], gals_arr['z'], np.ones_like(gals_arr['x'])))
 gals_pos_weights[:3] += boxsize/2 # shift coordinates from [-boxsize/2, boxsize/2] range to [0, boxsize]
